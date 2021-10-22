7813a893f3d3cf5e7c4880fbd83276ac2e8aa354
ir-sen <irsen.kan@bk.ru>
2021-10-22 12:53:47 +0300

    Create all fragment and binding

---
+++ app/src/main/java/com/example/brain/presentation/GameFinish.kt
@@ -0,0 +1,38 @@
+package com.example.brain.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.brain.R
+import com.example.brain.databinding.FragmentGameFinishBinding
+
+
+class GameFinish : Fragment() {
+
+    private var _binding: FragmentGameFinishBinding? = null
+    private val binding: FragmentGameFinishBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       _binding = FragmentGameFinishBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/brain/presentation/WelcomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.brain.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.brain.R
+import com.example.brain.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+    get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.understandBtn.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
